41545afb1dec4d585cf4bd00c40239ce00fea178
berny13 <bernyjimenezsanchez@gmail.com>
2021-01-25 20:28:58 +0100

    Se añade direccion de cuenta usuario

--- Sprint 6/redSocial/ey/urls.py
+++ Sprint 6/redSocial/ey/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.lobby, name='lobby'),
+    path('userConfiguration/', views.configuration, name='configuration'),
+    path('userInformation/', views.information, name='information'),
+
 ]
--- Sprint 6/redSocial/ey/views.py
+++ Sprint 6/redSocial/ey/views.py
@@ -5,4 +5,12 @@ from django.shortcuts import render
 # Create your views here.
 def lobby(request):
     context = {}
-    return render(request, 'Ey\Index.html', context=context)
+    return render(request, 'Ey/Index.html', context=context)
+
+def configuration(request):
+    context = {}
+    return render(request, 'userConfiguration/configuration.html', context=context)
+    
+def information(request):
+    context = {}
+    return render(request, 'userInformation/Information.html', context=context)
