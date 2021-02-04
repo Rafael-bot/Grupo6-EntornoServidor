5c85ae34f288b39af08d7a019d829c8d1e68e84b
AntonioVladimir <antortechin@gmail.com>
2021-02-04 19:14:57 +0100

    Corrección Sprint 6, añadiendo variables. También se han dinamizado las templates.

--- Sprint 7/redSocial/Cuentas/views.py
+++ Sprint 7/redSocial/Cuentas/views.py
@@ -1,29 +1,47 @@
 from django.shortcuts import render
 
 # Create your views here.
-def postPhoto(request):
-    context = {}
-    return render(request, 'Cuentas/postPhoto/postPhoto.html', context=context)
-
 def privateChat(request):
-    context = {}
+    context = {
+        'Usuario1':'Jose',
+        'Usuario2':'Lucrecia',
+        'Mensaje1':'Hola Lucrecia',
+        'Mensaje2': 'Hola Jose'
+    }
     return render(request, 'Cuentas/privateChat/privateChat.html', context=context)
 
 def configuration(request):
-    context = {}
+    context = {'Nombre':'Vladimir',
+               'Contraseña':'******',
+               'Email':'antortechin@gmail.com',
+               'Fotografía':'Photo',
+               'Teléfono':'666-666-666',
+               'Biography':'Hello, I am a backend programer'}
     return render(request, 'Cuentas/userConfiguration/configuration.html', context=context)
 
 
 def information(request):
-    context = {}
+    context = {'Nombre':'Vladimir',
+               'Contraseña':'******',
+               'Email':'antortechin@gmail.com',
+               'Fotografía':'Photo',
+               'Teléfono':'666-666-666',
+               'Biography':'Hello, I am a backend programer'}
     return render(request, 'Cuentas/userInformation/Information.html', context=context)
 
 
 def login(request):
-    context = {}
+    context = {
+        'username   ':'Jose',
+        'contrasenna': '123'
+    }
     return render(request, 'Cuentas/LoginRegister/Login.html', context=context)
 
 
 def register(request):
-    context = {}
+    context = {
+        'username':'Jose',
+        'contrasenna': '123',
+        'contrasenna_confirm': '123'
+    }
     return render(request, 'Cuentas/LoginRegister/Register.html', context=context)
--- Sprint 7/redSocial/Posts/views.py
+++ Sprint 7/redSocial/Posts/views.py
@@ -2,16 +2,32 @@ from django.shortcuts import render
 
 
 # Create your views here.
+def postPhoto(request):
+    context = {'Imagen': 'Imagen posteada'}
+    return render(request, 'Posts/postPhoto/postPhoto.html', context=context)
+
+
 def personalPosts(request):
-    context = {}
+    context = {
+        'foto': 'Fotografica post personal',
+        'username': 'Manuel',
+        'descripción': 'En el campo',
+    }
     return render(request, 'Posts/personalPosts/personalPosts.html', context=context)
 
 
 def publicPosts(request):
-    context = {}
+    context = {
+        'foto': 'Fotografica post público',
+        'username': 'Manuel',
+        'descripción': 'En la playa',
+    }
     return render(request, 'Posts/publicPosts/publicPosts.html', context=context)
 
 
 def comments(request):
-    context = {}
+    context = {
+        'username': 'Manuel',
+        'comentario': 'Bonita fotografía'
+    }
     return render(request, 'Posts/comments/comments.html', context=context)
