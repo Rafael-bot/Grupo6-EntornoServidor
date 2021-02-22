59a0c08176ff6b1409da2464491080093c253b7d
AntonioVladimir <antortechin@gmail.com>
2021-02-22 16:39:32 +0100

    Modifico modelos Post y Profile

--- Sprint 8/redSocial/Posts/models.py
+++ Sprint 8/redSocial/Posts/models.py
@@ -11,12 +11,12 @@ class Posts(models.Model):
     id_posts = models.CharField(max_length=80, unique=True, primary_key=True)
     number_posts = models.IntegerField()
     number_likes = models.IntegerField()
-    photo = models.ImageField(upload_to='posts')
+    photo = models.ImageField(upload_to='posts',blank=True,null=True)
     description = models.TextField(max_length=500, null=True)
     post_date = models.DateTimeField()
     public_or_private = models.BooleanField()
     username = models.ForeignKey('Cuentas.User', max_length=128,on_delete=models.CASCADE)
-    id_coments = models.ForeignKey('Coments.Coments',max_length=80, on_delete=models.CASCADE)
+    id_coments = models.ForeignKey('Coments.Coments',max_length=80, on_delete=models.CASCADE,null=True)
 
     def __str__(self):
         return self.id_posts
